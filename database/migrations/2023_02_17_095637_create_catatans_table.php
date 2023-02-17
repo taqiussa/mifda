bc0fa35bbb705a7b46929624a735ede2ec38303f
unknown <taqiussa>
2023-02-17 09:58:39 +0700

    migration ekstra prestasi catatan

---
+++ database/migrations/2023_02_17_095637_create_catatans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('catatans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('nis');
+            $table->foreignId('kelas_id');
+            $table->string('tahun',30);
+            $table->string('semester',3);
+            $table->text('catatan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('catatans');
+    }
+};
